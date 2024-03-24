eb2d6155eb741e14004ebff11462395541d31ab1
suganthi <suganthi.thangavelu@navinextventures.com>
2024-03-24 23:16:40 +0530

    Added model folder

---
+++ Model/WeatherForecast.cs
@@ -0,0 +1,8 @@
+﻿namespace MyWebApi.Model
+{
+    // Your record declaration goes here
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
