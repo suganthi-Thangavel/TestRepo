da87e649ce0aa8891fdb99a599020e8ed08f235e
suganthi <suganthi.thangavelu@navinextventures.com>
2024-03-24 23:11:43 +0530

    Added new controller

---
+++ Controller/ExceptionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExceptionController : ControllerBase
+    {
+    }
+}
